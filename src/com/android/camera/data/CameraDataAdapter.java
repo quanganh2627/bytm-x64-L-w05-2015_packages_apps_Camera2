7f084ced00b3b520469de348063e616ad18040fc
Rukai Wei <rukaix.wei@intel.com>
2014-03-06 09:32:21 +0800

    [PORT FROM R44B]Android Default Camera: close the cursor in time when add new pictures
    BZ: 175846
    
    Root Causes:
    Camera app didn't close the cursor in time after take picture, and use
    out all the file id of one thread in stress test.
    
    Change-Id: Idbf2c46ef24548d22da99a52af79a46ec1c26974
    Category: aosp improvement
    Domain: AOSP-Applications
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Rukai Wei <rukaix.wei@intel.com>

--- src/com/android/camera/data/CameraDataAdapter.java
+++ src/com/android/camera/data/CameraDataAdapter.java
@@ -149,6 +149,9 @@ public class CameraDataAdapter implements LocalDataAdapter {
             // A new data.
             insertData(newData);
         }
+        if (c != null) {
+            c.close();
+        }
     }
 
     // TODO: put the database query on background thread
@@ -170,6 +173,9 @@ public class CameraDataAdapter implements LocalDataAdapter {
         } else {
             // a new data.
             insertData(newData);
+        }
+        if (c != null) {
+            c.close();
         }
     }
 
